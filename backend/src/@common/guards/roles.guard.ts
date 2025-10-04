99689fb4a50f9100d54b60cd67ce7c8456d146e8
joaop06 <joaoofficialpedro@gmail.com>
2025-10-04 00:36:36 -0300

    Feat: Added decorator to parse query for find options

--- backend/src/@common/guards/roles.guard.ts
+++ backend/src/@common/guards/roles.guard.ts
@@ -1,7 +1,7 @@
 import { Reflector } from '@nestjs/core';
 import { RoleEnum } from '../enums/role.enum';
-import { ROLES_KEY } from '../decorators/roles.decorator';
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { ROLES_KEY } from '../decorators/roles/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
--- backend/src/applications/applications.controller.ts
+++ backend/src/applications/applications.controller.ts
@@ -8,14 +8,16 @@ import {
   UseGuards,
   Controller,
 } from '@nestjs/common';
+import { FindManyOptions } from 'typeorm';
 import { RoleEnum } from '../@common/enums/role.enum';
 import { RolesGuard } from '../@common/guards/roles.guard';
 import { Application } from './entities/application.entity';
 import { ApplicationsService } from './applications.service';
-import { Roles } from '../@common/decorators/roles.decorator';
 import { JwtAuthGuard } from '../@common/guards/jwt-auth.guard';
 import { CreateApplicationDto } from './dto/create-application.dto';
+import { Roles } from '../@common/decorators/roles/roles.decorator';
 import { UpdateApplicationDto } from './dto/update-application.dto';
+import { ParseQuery } from '../@common/decorators/query/parse-query.decorator';
 import { CreateApplicationUseCase } from './use-cases/create-application.use-case';
 import { DeleteApplicationUseCase } from './use-cases/delete-application.use-case';
 import { UpdateApplicationUseCase } from './use-cases/update-application.use-case';
@@ -30,13 +32,13 @@ export class ApplicationsController {
   ) { }
 
   @Get()
-  async findAll() {
-    return this.applicationsService.findAll();
+  async findAll(@ParseQuery() options: FindManyOptions<Application>) {
+    return await this.applicationsService.findAll(options);
   }
 
   @Get(':id')
   async findById(@Param('id') id: string): Promise<Application> {
-    return this.applicationsService.findById(+id);
+    return await this.applicationsService.findById(+id);
   }
 
   @Post()
@@ -55,13 +57,13 @@ export class ApplicationsController {
     @Param('id') id: string,
     @Body() updateApplicationDto: UpdateApplicationDto,
   ): Promise<Application> {
-    return this.updateApplicationUseCase.execute(+id, updateApplicationDto);
+    return await this.updateApplicationUseCase.execute(+id, updateApplicationDto);
   }
 
   @Delete(':id')
   @Roles(RoleEnum.Admin)
   @UseGuards(JwtAuthGuard, RolesGuard)
   async delete(@Param('id') id: string) {
-    return this.deleteApplicationUseCase.execute(+id);
+    return await this.deleteApplicationUseCase.execute(+id);
   }
 };
