cb9b9446bcdecca78b6c438d711da78d3812fd61
joaop06 <joaoofficialpedro@gmail.com>
2025-10-20 21:11:23 -0300

    Fix: circular imports

---
+++ backend/src/@common/types/entities.type.ts
@@ -0,0 +1,3 @@
+// Forward declarations to avoid circular dependencies
+export type ApplicationType = import('../../applications/entities/application.entity').Application;
+export type UserType = import('../../users/entities/user.entity').User;
--- backend/src/auth/dto/sign-in-response.dto.spec.ts
+++ backend/src/auth/dto/sign-in-response.dto.spec.ts
@@ -3,7 +3,7 @@ import { plainToInstance } from 'class-transformer';
 import { User } from '../../users/entities/user.entity';
 import { RoleEnum } from '../../@common/enums/role.enum';
 import { SignInResponseDto } from './sign-in-response.dto';
-import { Application } from '../../applications/entities/application.entity';
+import { ApplicationType } from '../../@common/types/entities.type';
 
 describe('SignInResponseDto', () => {
     it('should be defined', () => {
@@ -251,7 +251,7 @@ describe('SignInResponseDto', () => {
                     createdAt: '2023-01-01T00:00:00.000Z',
                     updatedAt: '2023-01-01T00:00:00.000Z',
                     deletedAt: null as unknown as Date,
-                    applications: [] as Application[],
+                    applications: [] as ApplicationType[],
                 },
             };
 
